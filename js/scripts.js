f31cd001b0cf23e2ab7b26839d91cf7edd758596
Kyra Oja <k.oja@comcast.net>
2014-02-20 14:30:36 -0800

    headers

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	var msg1 = 'Hello!';
+	var msg2 = 'Hola!';
+	var msg3 = 'Bonjour!';
+
+
+
+
+	$('h1').text(msg2);
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
