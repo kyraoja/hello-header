0bb0f0717f0c61d38fd95b9f6cde7b6b288b8e35
Kyra Oja <k.oja@comcast.net>
2014-02-20 14:52:08 -0800

    doesnt work

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,19 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	var msg1 = 'Hello!';
-	var msg2 = 'Hola!';
-	var msg3 = 'Bonjour!';
+	// returns 0 to 'limit'
+var limit = 2;
+var numRand = Math.floor(Math.random()*limit);
+
+	var msg0 = 'Hello!';
+	var msg1 = 'Hola!';
+	var msg2 = 'Bonjour!';
 
 
 
 
-	$('h1').text(msg2);
+	$('h1').text('msg' + numRand);
 
-	console.log('Page Loaded. Lets Do this!');
+	console.log('msg' + numRand);
 
 }); 
